0991a662cdf0129f778ee1566dcddd071354424e
jgrk88 <jacobkaplan88@gmail.com>
2021-01-27 23:11:37 -0500

    Refactor utilizing JSON library

--- A2/src/NumJSONParser.java
+++ A2/src/NumJSONParser.java
@@ -1,140 +1,41 @@
 package src;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import java.util.ArrayList;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.lang.IllegalArgumentException;
+import org.json.JSONTokener;
 
 public class NumJSONParser {
 
-	//Returns the number of characters until the starting "open character" is closed by
-	//its corresponding "close character"
-	public static Integer findLength(BufferedReader input, int openCode, int closeCode) {
-		//Always start with one brace
-		int count = 1;
-		int openBraces = 1;
-
-		try {
-			//Skipping the original brace
-			input.skip(1);
-			//If there are no unclosed braces remaining end loop
-			while (openBraces > 0) {
-				int currChar = input.read();
-				count++;
-				if (currChar == openCode) {
-					//open another brace
-					openBraces++;
-				} else if (currChar == closeCode) {
-					//close latest brace
-					openBraces--;
-				}
-			}
-			input.reset();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return count;
-	}
-
-	//Breaks series of NumJSONs (as strings) into a list to iterate over
-	public static ArrayList<String> parseInput(BufferedReader input) {
-		ArrayList<String> output = new ArrayList<>();
-		try {
-			input.mark(1024);
-			int currChar = 0;
-			//Read first char and reset cursor to marker
-			//Once char is -1 or EOF end loop
-			while((currChar = input.read()) != -1) {
-				input.reset();
-				if (currChar == 32) {
-					//If space skip
-					input.skip(1);
-					input.mark(1024);
-				} else if (currChar == 123) {
-					//If curly bracket read to end of curly bracket
-					int length = findLength(input, 123, 125);
-					char[] buffer = new char[length];
-					input.read(buffer, 0, length);
-					input.mark(1024);
-					output.add(new String(buffer));
-				} else if (currChar == 91) {
-					//If square bracket read to end of square bracket
-					int length = findLength(input, 91, 93);
-					char[] buffer = new char[length];
-					input.read(buffer, 0, length);
-					input.mark(1024);
-					output.add(new String(buffer));
-				} else {
-					//Else read until next space or EOF
-					int length = 0;
-					while ((currChar = input.read()) != -1 && currChar != 32) {
-						length++;
-					}
-					input.reset();
-
-					char[] buffer = new char[length];
-					input.read(buffer, 0, length);
-					input.mark(1024);
-					output.add(new String(buffer));
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return output;
-	}
-	
-	//Determines whether the given string is an integer value
-	//Returns the corresponding boolean
-	public static Boolean isNum(String s) {
-		try {
-			Integer.parseInt(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-	
-	//Creates and returns a JSON object for a key-value pair
-	public static JSONObject appendToJSON(String object, Integer total) {
-		JSONObject outputJSON = new JSONObject();
-		outputJSON.put("total", total);
-		outputJSON.put("object", object);
-		return outputJSON;
-	}
-	
-	//Iterates over a given array and performs the corresponding operation
-	//Returns the total result for that given operation 
-	public static Integer computeOp(String s, String operation) {
-		Integer total = 0;
-		
-		//Splits the string on spaces, commas, and square brackets
-		String[] values = s.split("\\s*(\\s|,|\\]|\\[)\\s*");
+	public static int recursiveOp(Object value, String operation) {
+		int ident;
 		if (operation.equals("sum")) {
-			for (String v: values) {
-				if (isNum(v)) {
-					total = total + Integer.parseInt(v);
+			ident = 0;
+		} else {
+			ident = 1;
+		}
+		if (value instanceof Integer) {
+			return (Integer)value;
+		} else if (value instanceof String) {
+			return ident;
+		} else if (value instanceof JSONArray) {
+			JSONArray jarr = (JSONArray)value;
+			int total = ident;
+			for (int ii = 0; ii < jarr.length(); ii++) {
+				if (operation.equals("sum")) {
+					total = total + recursiveOp(jarr.get(ii), operation);
+				} else {
+					total = total * recursiveOp(jarr.get(ii), operation);
 				}
 			}
+			return total;
+		} else if (value instanceof JSONObject) {
+			JSONObject jobj = (JSONObject)value;
+			return recursiveOp(jobj.get("payload"), operation);
+		} else {
+			throw new IllegalArgumentException("Input must be only integers, strings, arrays, and json objects");
 		}
-		if (operation.equals("product")) {
-			total = 1;
-			for (String v: values) {
-				if (isNum(v)) {
-					total = total * Integer.parseInt(v);
-				}
-			}
-		}
-		return total;
 	}
 
 	public static void main(String[] args) {
@@ -158,27 +59,22 @@ public class NumJSONParser {
 		} catch (ArrayIndexOutOfBoundsException e) {
 			throw new ArrayIndexOutOfBoundsException("Please enter a valid operation: --sum or --product");
 		}
-		
-		
-		BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<String> chunks = parseInput(inputStream);
-		
-		JSONArray output = new JSONArray();
 
-		for (String s : chunks) {
-			//If the value in the ArrayList is an Object, process the object and add it to the output
-			if (s.contains("{") || s.contains("}")) {
-				//TO-DO
-				//output.put(appendToJSON(s, processObject(s, operation)));
+		JSONArray outputArray = new JSONArray();
+		JSONTokener inputTokens = new JSONTokener(System.in);
+		try {
+			while (true) {
+				Object value = inputTokens.nextValue();
+				JSONObject outputJSON = new JSONObject();
+				outputJSON.put("total", recursiveOp(value, operation));
+				outputJSON.put("object", value);
+				outputArray.put(outputJSON);
 			}
-			//If the value is an array, a singular number, or a string, compute the total and add it to the output
-			else {
-				output.put(appendToJSON(s, computeOp(s, operation)));
-			}
+		} catch (org.json.JSONException e) {
+
 		}
-		
-		System.out.println(output);
-		
+
+		System.out.println(outputArray);
 	}
 	
 
